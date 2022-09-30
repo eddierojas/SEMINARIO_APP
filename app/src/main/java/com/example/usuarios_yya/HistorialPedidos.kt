7606289924dc45753ca5bcd3f1583887667b711b
jguerra <jguerrag1@miumg.edu.gt>
2022-09-29 22:09:18 -0600

    Menu_Usuario

---
+++ app/src/main/java/com/example/usuarios_yya/HistorialPedidos.kt
@@ -0,0 +1,11 @@
+package com.example.usuarios_yya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistorialPedidos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historial_pedidos)
+    }
+}
--- app/src/main/java/com/example/usuarios_yya/HomeActivity.kt
+++ app/src/main/java/com/example/usuarios_yya/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.example.usuarios_yya
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -15,6 +17,8 @@ enum class ProviderType{
     BASIC
 }
 class HomeActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -24,6 +28,37 @@ class HomeActivity : AppCompatActivity() {
         val provider= bundle?.getString("provider")
 
         setup(email?:"", provider?:"")
+
+findViewById<CardView>(R.id.card1).setOnClickListener{
+    val intent = Intent(this, Tienda::class.java)
+    startActivity(intent)
+
+}
+
+        findViewById<CardView>(R.id.card2).setOnClickListener{
+            val intent = Intent(this, Pedidos::class.java)
+            startActivity(intent)
+
+        }
+
+        findViewById<CardView>(R.id.card3).setOnClickListener{
+            val intent = Intent(this, HistorialPedidos::class.java)
+            startActivity(intent)
+
+        }
+
+        findViewById<CardView>(R.id.card4).setOnClickListener{
+            val intent = Intent(this, Perfil::class.java)
+            startActivity(intent)
+
+        }
+
+        findViewById<CardView>(R.id.card5).setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+
+        }
+
     }
 
     private fun setup(email: String, provider:String){
@@ -33,10 +68,7 @@ class HomeActivity : AppCompatActivity() {
         emailU.text=email;
         prov.text=provider
 
-        findViewById<Button>(R.id.logout).setOnClickListener{
-            FirebaseAuth.getInstance().signOut()
-            onBackPressed()
-        }
+
 
     }
 }
